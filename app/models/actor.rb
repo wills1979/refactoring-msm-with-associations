a2b02e67b0a7859e0ced8940a65979d9a950e654
wills1979 <107662428+wills1979@users.noreply.github.com>
2025-05-09 21:07:56 +0000

    add associations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -13,6 +13,6 @@
 class Actor < ApplicationRecord
   validates(:name, presence: true)
 
-  # not sure about this
-  # has_many(:characters, class_name: "Character", foreign_key: "actor_id", primary_key: "id")
+  has_many(:characters, class_name: "Character", foreign_key: "actor_id", primary_key: "id")
+  has_many(:filmography, through: :characters, source: :movie)
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -17,5 +17,6 @@ class Movie < ApplicationRecord
   validates(:title, uniqueness: true)
 
   belongs_to(:director, class_name: "Director", foreign_key: "director_id", primary_key: "id")
-  has_many(:characters, class_name "Character", foreign_key: "actor_id", primary_key: "id")
+  has_many(:characters, class_name: "Character", foreign_key: "actor_id", primary_key: "id")
+  has_many(:cast, through: :characters, source: :actor)
 end
